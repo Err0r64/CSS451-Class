7effa86527239157b2276de6d6d283791ab7e4c0
Kelvin Sung <KSung@uwb.edu>
2025-09-29 11:58:03 -0700

    New WFP example

---
+++ ClassExamples/Topic2-UI+MVC/2.1.WPF_Analogy/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int v = 0;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            v++;
+            label.Content = "V=" + v.ToString();
+        }
+    }
+}
