3b0e754c9425ba3e033a50b20b8e4776c3a6dc03
Kelvin Sung <ksung@uw.edu>
2025-10-15 10:02:35 -0700

    Update Coding Exercise to something more reasonable.

--- ClassExamples/Topic3-VectorMath+Rotation/Topic3-InClassCodingExercises/Assets/PlaneExercise.cs
+++ ClassExamples/Topic3-VectorMath+Rotation/Topic3-InClassCodingExercises/Assets/PlaneExercise.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class PlaneExercise : MonoBehaviour
 {
     public Transform WhitePlane;
     public Transform RedQuad;
-        // Note: by default RedQuad's normal is in the -forward direction
+    // Note: by default RedQuad's normal is in the -forward direction
 
+    public bool ShowWhite = true;
+    public bool ShowRed = false;
     public Vector3 WhitePlaneNormal = new Vector3(0, 1, 0);
     public float WhitePlaneD = 0; // Ax + By + Cz + D = 0
 
     private GameObject RedQuadNormal = null;
     private GameObject RedQuadD = null;
 
-    private const float kNormalSize = 2f;
+    private const float kNormalSize = 4f;
     
 
     // Start is called before the first frame update
@@ -26,7 +29,7 @@ public class PlaneExercise : MonoBehaviour
 
         RedQuadNormal = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
         RedQuadNormal.GetComponent<Renderer>().material.color = Color.blue; 
-        RedQuadNormal.transform.localScale = new Vector3(0.5f, kNormalSize, 0.5f);
+        RedQuadNormal.transform.localScale = new Vector3(0.2f, kNormalSize, 0.2f);
         RedQuadNormal.SetActive(false);
         RedQuadD = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         RedQuadD.GetComponent<Renderer>().material.color = Color.black;
@@ -37,14 +40,42 @@ public class PlaneExercise : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Assuming the equation is:
+        //      n  dot  P =  D
+        //      n  dot  P - D = 0
+        // and not:
+        //      n dot P + D = 0
+
         // 1. Display the whiteplane according to the normal and D
-        
+        WhitePlane.gameObject.SetActive(ShowWhite);
+        if (ShowWhite)
+        {
+            // ...
+            // Do you know how to draw a cylinder on the white plane to show its normal?
+        }
 
         // 2. draw the normal of the red quad and its D 
         //    D: value sets the sphere radius (sphere should locate at Quad center)
-        // AI says:
-        RedQuadD.SetActive(true);
-        RedQuadNormal.SetActive(true);
+        RedQuadD.SetActive(ShowRed);
+        RedQuadNormal.SetActive(ShowRed);
+        RedQuad.gameObject.SetActive(ShowRed);
+        if (ShowRed)
+        {
+            // ...
+        }
 
+    }
+
+        // Implement this function: draws cylinder from p1 to p2
+    void SetLine(Vector3 p1, Vector3 p2, GameObject line)
+    {
+        line.SetActive(true);
+        Vector3 v = p2 - p1;
+        Vector3 vn = v.normalized;
+        float length = v.magnitude;
+        line.transform.position = p1 + length * 0.5f * vn; // or (p1 + p2) / 2;
+        line.transform.up = vn;
+        Vector3 scale = line.transform.localScale;
+        line.transform.localScale = new Vector3(scale.x, length / 2f, scale.z);
     }
 }
--- ClassExamples/Topic3-VectorMath+Rotation/Topic3-InClassCodingExercises/Assets/VectorExercise.cs
+++ ClassExamples/Topic3-VectorMath+Rotation/Topic3-InClassCodingExercises/Assets/VectorExercise.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class VectorExercise : MonoBehaviour
@@ -49,43 +50,46 @@ public class VectorExercise : MonoBehaviour
         GreenLine.GetComponent<Renderer>().material.color = Color.green;
         GreenLine.transform.localScale = new Vector3(0.01f, 0.5f, 0.01f);
         GreenLine.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 1. March objA between p1 and p2 at speed ObjASpeed
-        // AI says: 
-        ObjA.position = Vector3.Lerp(p1.localPosition, p2.localPosition, Mathf.PingPong(Time.time * ObjASpeed, 1));
-        // now ignore the above and perform this with vectors we learned in class
+        // 1. Continuously marching ObjA towards p2, if reach P2, start over at P1
+
 
         // 2. send the Projectile towards Red at speed ProjectileSpeed when space is pressed
         //    projectile should start at ObjA's position and disappear when it reaches Red
-        // AI says:
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // send projectile towards Red
+            Projectile.SetActive(true);
+            // ... 
         }
+
         if (Projectile.activeSelf)
         {
-            // switch off when sufficiently close to Red
-            //  Projectile.SetActive(false);
-
+            // ...
         }
 
-        // 3. March Red between p3 and p4 at speed RedSpeed
-        // AI says:
-        Red.localPosition = Vector3.Lerp(p3.localPosition, p4.localPosition, Mathf.PingPong(Time.time * RedSpeed, 1));
-        // now ignore the above and perform this with vectors we learned in class
+        // 3. March Red along the direction defined by P3 towards P4, when Red is 
+        //    farther than 10 units from p3, it should reset to p3 and repeat
+        // ...
+        
+        
 
-        // 4. Draw the two lines between P1 and P2 and between p3 and p4
-        SetTheLine(p1.localPosition, p2.localPosition, WhiteLine);
-        SetTheLine(p3.localPosition, p4.localPosition, GreenLine);
+        // 4. Define a SetLine function to receive three parameters
+        //       SetLine(Vector3 p1, Vector3 p2, transform CylinderTransform)
+        //    and draws the cylinder from p1 to p2
+        // Now draw the lines between p1 to p2, and p3 to p4
+        SetLine(p1.localPosition, p2.localPosition, WhiteLine);
+        SetLine(p3.localPosition, p4.localPosition, GreenLine);
 
     }
     
     // Implement this function: draws cylinder from p1 to p2
-    void SetTheLine(Vector3 p1, Vector3 p2, GameObject line)
+    void SetLine(Vector3 p1, Vector3 p2, GameObject line)
     {
-    }   
+        
+    }
 }
