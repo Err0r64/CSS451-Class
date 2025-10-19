7b4f247063f478bc665a2f8d39d87c47c081f61a
Kelvin Sung <ksung@uw.edu>
2025-10-19 13:09:22 -0700

    New coding exercise

---
+++ ClassExamples/Topic3-VectorMath+Rotation/Topic3-InClassCodingExercises/Assets/EX5_ReflectionExercise.cs
@@ -0,0 +1,85 @@
+﻿
+using UnityEngine;
+
+public class EX5_ReflectionExercise : MonoBehaviour
+{
+    public Transform WhitePlane;
+
+    public Transform P1; // Point P1 of line
+    public Transform P2; // Point P2 of line
+    public Transform Pt; // Pt, line intersect with Plane
+                         //      to show the reflection point on the plane
+
+
+    public Transform AxisFrame = null;
+    public bool ShowAxisFrame = true;
+
+    public bool ShowAllLines = false;
+    // 
+    private GameObject PlaneNormalLine = null;  // normal of the plane
+    
+    private GameObject LineP1P2 = null; // shows the line from P1 to P2
+    private GameObject ReflectionLine = null; // shows the reflection line
+    private GameObject VectorM = null; // perpendicular vector from P1 to the normal vector
+    private GameObject NormalAtPt = null; // draws the normal vector at Pt
+    
+        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(WhitePlane != null);
+        Debug.Assert(P1 != null);
+        Debug.Assert(P2 != null);
+        Debug.Assert(Pt != null);
+        Debug.Assert(AxisFrame != null);
+
+        PlaneNormalLine = Support.CreateLine("PlaneNormal", Color.red);
+        PlaneNormalLine.SetActive(false);
+
+        LineP1P2 = Support.CreateLine("LineP1P2", Color.white);
+        ReflectionLine = Support.CreateLine("ReflectionLine", Color.green);
+
+        VectorM = Support.CreateLine("VectorM", Color.yellow);
+        VectorM.SetActive(ShowAllLines);
+        NormalAtPt = Support.CreateLine("NormalAtPt", Color.red); 
+        NormalAtPt.SetActive(ShowAllLines);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AxisFrame.gameObject.SetActive(ShowAxisFrame);
+
+        // Assuming the equation is:
+        //      n  dot  P =  D
+        //      n  dot  P - D = 0
+        // and not:
+        //      n dot P + D = 0
+        Vector3 n = WhitePlane.up;
+
+        // 1. Update the whiteplane normal
+        Support.UpdatePlaneNormal(WhitePlane, n, PlaneNormalLine);
+        Support.UpdateLine(P1.localPosition, P2.localPosition, LineP1P2);
+
+        // Line form by P1 to P2
+        // Intersection this line with WhitePlane: at Pt
+        // Reflect the line at Pt across the plane normal
+        //
+        // One step at a time, when done with each step, 
+        //       make sure answer is correct
+        //       test thoroughly by moving P1, P2 and the plane around 
+        // Next steps to do:
+        //   1. Compute and show Pt, the intersection point of line and plane
+        //   2. Compute the reflection direction, show as line (will show at origin)
+        //   3. Compute reflection at Pt
+        //   4. Reflect only if P2 is in front of the plane
+        //   5. Set P2 color to Green if reflected, Red if not reflected
+        //   
+        // The lines: start drawing as soon as possible to show correct
+        //            worry about user control of line visibility last
+        
+
+
+    }
+}
---
+++ ClassExamples/Topic3-VectorMath+Rotation/Topic3-InClassCodingExercises/Assets/EX6_ProjectToCylinderExercise.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class EX6_ProjectToCylinderExercise : MonoBehaviour
+{
+    public Transform P; // Point to project to the cylinder
+
+    public Transform Pt; // to show the projected point on the cylinder
+    public Transform TheCylinder;
+    public float CylinderRadius = 1.0f;
+    public Transform CP1; // Cylinder Point 1 (top)
+    public Transform CP2; // Cylinder Point 2 (bottom)
+
+    private GameObject ProjectionLine = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(P != null);
+        Debug.Assert(Pt != null);
+        Debug.Assert(TheCylinder != null);
+
+        
+        // The lines
+        ProjectionLine = new GameObject("ProjectionLine");
+        ProjectionLine = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        ProjectionLine.GetComponent<Renderer>().material.color = Color.white;
+        ProjectionLine.transform.localScale = new Vector3(0.1f, 0.5f, 0.1f);
+        ProjectionLine.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1. Set TheCylinder's scale according to CylinderRadius
+        //    Note: TheCylinder's original radius is 0.5 (default Unity cylinder)
+        TheCylinder.localScale = new Vector3(2.0f*CylinderRadius, TheCylinder.localScale.y, 2.0f*CylinderRadius);
+
+        Vector3 vn = TheCylinder.up;  // Cylinder direction is v, vn says it is normalized
+        float h = 2.0f * TheCylinder.localScale.y; // half height of the cylinder
+
+        // 2. P1 and P2 of the cylinder: 
+        //      remember Cylinder is centered at its localPosition
+        //      and its object space height is 2.0
+        CP1.transform.localPosition = TheCylinder.localPosition - 0.5f * h * vn;
+        CP2.transform.localPosition = TheCylinder.localPosition + 0.5f * h * vn;
+
+        // Now, project P to the cylinder surface
+        //    if projected point is outside the height range, color it red, else blue
+        // Pt is the projected point on the cylinder surface
+    
+    }
+}
